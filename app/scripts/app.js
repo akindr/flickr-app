ca3ecb799d01250bbe03490bb5a6903069c75607
Austin Kindred <austin.kindred@gmail.com>
2015-02-20 17:04:07 -0500

    Removed TODO

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,10 +1,8 @@
 /**
  Handles boostrapping the angular stuff via Angular AMD
  **/
-
 require(["angular", "angular-route", "angular-animate", "bootstrap", "underscore",
     // Our scripts -- Add to this as we create new views and services
-    // TODO: Add services and directive
     "scripts/module", "scripts/photos/photo-ctrl", "scripts/map/map-ctrl", "scripts/stats/stats-ctrl"],
 
     // Bootstrap the angular app
