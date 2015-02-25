1814a3c2956c44a29f53543b8e424d34028154ea
Austin Kindred <austin.kindred@gmail.com>
2015-02-24 19:52:08 -0500

    Added the require for the empty flickr service file

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,11 +1,15 @@
+"use strict"
+
 /**
- Handles boostrapping the angular stuff via Angular AMD
- **/
-
-require(["angular", "angular-route", "angular-animate", "bootstrap", "underscore",
-    // Our scripts -- Add to this as we create new views and services
-    "scripts/module", "scripts/photos/photo-ctrl", "scripts/map/map-ctrl", "scripts/stats/stats-ctrl"],
-
+ * Require all of our application code, and bootstrap angular once we have it all
+ */
+require(
+    [
+        "angular", "angular-route", "angular-animate", "bootstrap", "underscore",
+        // Our scripts -- Add to this as we create new views and services
+        "scripts/module", "scripts/photos/photo-ctrl", "scripts/map/map-ctrl", "scripts/stats/stats-ctrl",
+        "scripts/services/flickr-service"
+    ],
 
     // Bootstrap the angular app
     function(angular){
